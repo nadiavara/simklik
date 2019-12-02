4a5ceff39a0c290206c44093a73f34eda6e8c85c
nadiavara <43781940+nadiavara@users.noreply.github.com>
2019-12-02 18:50:28 +0700

    Pertemuan 9

---
+++ app/src/main/java/id/kuliah/simklik/MainActivity7.kt
@@ -0,0 +1,12 @@
+package id.kuliah.simklik
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity7 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main7)
+    }
+}
